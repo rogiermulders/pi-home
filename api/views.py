8babf08c75b14cf2ccebacdc7546055fedafa679
Rogier Mulders <rogier.mulders@xs4all.nl>
2025-08-17 09:01:24 +0200

    Oops

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 class HomeView(APIView):
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         return Response({"message": "This is a protected route"})
